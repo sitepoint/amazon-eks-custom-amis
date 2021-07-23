b9e496cf913022a27d80f06ddab356df9b80da8f
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-07-23 17:44:05 -0400

    Update airgap images

--- files/gitpod/airgap.sh
+++ files/gitpod/airgap.sh
@@ -6,16 +6,9 @@ set -o errexit
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 
+# Extract images from nodes:
+# kubectl get nodes -o json | jq --raw-output '.items[].status.images[].names | .[]'
 IMAGES=$(cat "${DIR}/airgap-images.txt")
 
 # Download images
 xargs -n1 nerdctl --namespace k8s.io pull <<< "${IMAGES}"
-
-# Export images to tar file
-nerdctl --namespace k8s.io save -o /airgap-images.tar $(echo "${IMAGES[*]}")
-
-# Compress tar file
-gzip -v -c /airgap-images.tar > /airgap-images.tar.gz
-
-# Cleanup
-rm /airgap-images.tar
